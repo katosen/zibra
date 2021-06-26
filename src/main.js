699c2b65bebf456a545d2736153b42f76c7eddcb
harriszhang <harriszhang@live.cn>
2021-06-26 11:02:29 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import { createApp } from 'vue'
-import vuetify from './plugins/vuetify'
+import { createVuetify } from 'vuetify'
 import App from './App.vue'
+
+import 'vuetify/dist/vuetify.css'
 
 const app = createApp(App)
 
-const vuetify = createVuetify(...) 
-app.use(vuetify)
+const vuetify = createVuetify() 
 app.use(vuetify)
 
 app.mount('#app')
